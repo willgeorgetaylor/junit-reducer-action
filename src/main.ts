bffa5782522f5faf60ffa336b5442437cc7d7f91
Will Taylor <willgeorgetaylor@gmail.com>
2023-12-28 23:59:41 +0000

    fix: exclude version from passthrough

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,9 @@ function enumerateInputs(): Record<string, string> {
     if (key.startsWith('INPUT_')) {
       const inputName = key.slice('INPUT_'.length).toLowerCase()
       const inputValue = process.env[key]
-      inputs[inputName] = inputValue || ''
+      if (inputName !== 'version') {
+        inputs[inputName] = inputValue || ''
+      }
     }
   }
   return inputs
